499b2f4f67a4a956d31966b022f9b9f174f5bf71
IntelligentMegafauna <61617962+IntelligentMegafauna@users.noreply.github.com>
2020-03-07 19:29:28 -0800

    Update NBAMenu.py

--- NBAMenu.py
+++ NBAMenu.py
@@ -12,6 +12,32 @@ def printNBA():
     print("3. Lebron James")
     print("4. Kobe Bryant")
 
+def printMovie():
+    print("1. The Blue Pill")
+    print("2. The Red Pill")
+
+def printPolitics():
+    print("1. William Mckinley")
+    print("2. Woodrow Wilson")
+    print("3. Herbert Hoover")
+    print("4. Calvin Coolidge")
+
+def checkAnswer(answerToQuestion):
+    questionResponse = input("\nYour Selection: ")
+    try:
+        questionResponse = int(questionResponse)
+    except:
+        questionResponse = 0
+
+    if(questionResponse == answerToQuestion):
+        print("\nThat is correct.")
+    elif(questionResponse != answerToQuestion and questionResponse != 0):
+        print("\nSorry. That is incorrect.")
+    elif(questionResponse == 0):
+        print("[[Unable to convert response to a number.]]")
+    else:
+        print("[[Unexpected Input Error]]")
+
 categoryResponse = 0
 
 while categoryResponse >=0 and categoryResponse <=5 and categoryResponse !=6:
@@ -28,20 +54,7 @@ while categoryResponse >=0 and categoryResponse <=5 and categoryResponse !=6:
         print("\nWhat NBA player had the highest AVG PPG:")
         printNBA()
 
-        questionResponse = input("\nYour Selection: ")
-        try:
-            questionResponse = int(questionResponse)
-        except:
-            questionResponse = 0
-
-        if(questionResponse == 1):
-            print("\nThat is correct.")
-        elif(questionResponse != 1 and questionResponse != 0):
-            print("\nSorry. That is incorrect.")
-        elif(questionResponse == 0):
-            print("[[Unable to convert response to a number.]]")
-        else:
-            print("[[Unexpected Input Error]]")
+        checkAnswer(1)
 
     elif(categoryResponse == 2):
         print("\n[Triva Question for Movies]")
@@ -51,7 +64,7 @@ while categoryResponse >=0 and categoryResponse <=5 and categoryResponse !=6:
         print("\n[Overwatch Question for Movies]")
     elif(categoryResponse == 5):
         print("\n[Random Question for Movies]")
-    elif(categoryResponse == 5):
+    elif(categoryResponse == 6):
         print("\nThanks for Playing. Goodbye.")
     else:
         print("\n[[Invalid Input Error]]")
